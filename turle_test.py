7db869b935705790804081837e8720c1ce0ab5c0
ddingyull <105038512+ddingyull@users.noreply.github.com>
2022-06-21 09:32:34 +0900

    Add files via upload

---
+++ turle_test.py
@@ -0,0 +1,41 @@
+import turtle as t
+t.shape("turtle")
+#삼각형그리기
+t.color("pink")
+t.fd(100)
+t.lt(120)
+t.fd(120)
+t.lt(120)
+t.fd(100)
+t.fd(20)
+t.lt(120)
+t.fd(20)
+ 
+#사각형그리기
+t.pensize(4)
+t.fd(100)
+t.lt(90)
+t.fd(120)
+t.lt(90)
+t.fd(120)
+t.lt(90)
+t.fd(120)
+t.lt(90)
+
+#원 그리기
+t.color("skyblue")
+t.circle(50)
+t.color("turquoise")
+t.circle(60)
+
+#변수사용해서 만들기
+import turtle as t
+d=100
+e=120
+t.fd(d)
+t.fd(d)
+t.lt(e)
+t.fd(d)
+t.lt(e)
+t.fd(d)
+t.lt(e)
